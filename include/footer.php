41ead0a6ae357450d9db354cdfa1f688e70e9079
Florian <116985718+Galeih@users.noreply.github.com>
2023-05-23 09:53:34 +0200

    Review include files

--- include/footer.php
+++ include/footer.php
@@ -9,7 +9,7 @@ $file = $parts[count($parts) - 1];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="include/footer.css">
 </head>
 <body>
 <footer>
