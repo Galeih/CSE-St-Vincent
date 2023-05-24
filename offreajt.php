b8f37b2cf90ffcb9e09e95a6207c8b68c4cc561e
Florian Sauvage <florian.sauvage.etudiant@gmail.com>
2023-05-25 01:03:24 +0200

    HELO

--- offreajt.php
+++ offreajt.php
@@ -4,12 +4,17 @@ require 'include/connectionbdd.php';
 if (empty($_GET) === false) {
     var_dump($_GET);
     $erreurs = [];
-	if (empty($_GET['lien'])) {
-		$erreurs['lien'] = 'Veuillez saisir un lien.';
+	if (empty($_GET['dateDebut'])) {
+		$erreurs['dateDebut'] = 'Veuillez saisir un Date_Debut_Offre.';
+	} 
+	if (empty($_GET['dateFin'])) {
+		$erreurs['dateFin'] = 'Veuillez saisir un Date_Fin_Offre.';
+	} 
+	if (empty($_GET['nbMinPlace'])) {
+		$erreurs['nbMinPlace'] = 'Veuillez saisir un Nombre_Place_Min_Offre.';
 	} 
 	
 	$expressionReguliere = '/[\d\'\/~`\!@#\$%\^&\*\(\)_\-\+=\{\}\[\]\|;:"\<\>,\.\?\\\]/';
-
 
 	if (empty($_GET['description'])) {
 		$erreurs['description'] = 'Veuillez saisir une description.';
@@ -25,25 +30,16 @@ if (empty($_GET) === false) {
 		}
 	}
 
-    if (empty($_GET['image'])) {
-		$erreurs['image'] = 'Veuillez saisir un nom pour une image.';
-	} 
-
     if (empty($erreurs)) {
-        try{
-            $requeteInsertion1= $connexion->prepare('INSERT INTO images (Nom_Image) VALUES (:Nom_Image)');
-            $requeteInsertion1->bindParam('Nom_Image', $_GET['image']);
-            $requeteInsertion1->execute();
-        } catch (\Exception $exception) {
-            echo $exception;
-        }
+
         try {
             $idImg =  $connexion->lastInsertId();
-            $requeteInsertion = $connexion->prepare('INSERT INTO partenaire (Nom_Partenaire, Description_Partenaire, Lien_Partenaire, Id_Image) VALUES (:Nom_Partenaire, :Description_Partenaire, :Lien_Partenaire, :idimg)');
+            $requeteInsertion = $connexion->prepare('INSERT INTO offre (Nom_Offre, Description_Offre, Date_Debut_Offre, Date_Fin_Offre, Nombre_Place_Min_Offre) VALUES (:Nom_Offre, :Description_Offre, :Date_Debut_Offre, :Date_Fin_Offre, :Nombre_Place_Min_Offre)');
             $requeteInsertion->bindParam('Nom_Partenaire', $_GET['nom']);
-            $requeteInsertion->bindParam('Description_Partenaire', $_GET['description']);
-            $requeteInsertion->bindParam('Lien_Partenaire', $_GET['lien']);
-            $requeteInsertion->bindParam('idimg', $idImg);
+            $requeteInsertion->bindParam('Description_Offre', $_GET['description']);
+            $requeteInsertion->bindParam('Date_Debut_Offre', $_GET['dateDebut']);
+            $requeteInsertion->bindParam('Date_Fin_Offre', $_GET['dateFin']);
+            $requeteInsertion->bindParam('Nombre_Place_Min_Offre', $_GET['nbMinPlace']);
 
             $requeteInsertion->execute();
 
@@ -72,7 +68,7 @@ if (empty($_GET) === false) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link  rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap">
         <link rel="icon" href="assets/sv_logo.png">
-        <title>CSE Saint-Vincent - Partenariats</title>
+        <title>CSE Saint-Vincent - Back - AjouterPartenaire</title>
     </head>
     <body>
         <header>
@@ -83,7 +79,7 @@ if (empty($_GET) === false) {
                     <div class="logo"><img src="assets/logo_st_vincent_1.png" alt="logo_st_vincent"></div>
                     <ul>
                     <a href="back.php">
-                            <li class="active">
+                            <li>
                                 Back-Accueil
                             </li>
                         </a>
@@ -106,36 +102,46 @@ if (empty($_GET) === false) {
                 </nav>
             </div>
         </header>
+
         <main>
-            <form id="formulaire_ajt" action="partajt.php" method="GET">
-                <div>
+            <form id="formulaire_ajt" action="offreajt.php" method="GET" class=""
+            style="display: flex; flex-direction: column; justify-content: center; align-items: center; padding: 20px; border-radius: 5px; border: 1px solid #a39677">
+                <div style="padding: 10px;">
                     <label for="nom">Nom :</label>
                     <input type="text" name="nom" value="<?= isset($_GET['nom']) ? $_GET['nom'] : null; ?>">
                     <?= isset($erreurs['nom']) ? $erreurs['nom'] : null; ?>
                 </div>
 
-                <div>
+                <div style="padding: 10px;">
                     <label for="description">Description :</label>
-                    <input type="text" name="description" value="<?= isset($_GET['description']) ? $_GET['description'] : null; ?>">
+                    <textarea name="description" value="<?= isset($_GET['description']) ? $_GET['description'] : null; ?>">
                     <?= isset($erreurs['description']) ? $erreurs['description'] : null; ?>
+                    </textarea>
                 </div>
 
-                <div>
-                    <label for="lien">Lien :</label>
-                    <input type="text" name="lien" value="<?= isset($_GET['lien']) ? $_GET['lien'] : null; ?>">
-                    <?= isset($erreurs['lien']) ? $erreurs['lien'] : null; ?>
+                <div style="padding: 10px;">
+                    <label for="date">Date début offre :</label>
+                    <input type="date" name="dateDebut" value="<?= isset($_GET['dateDebut']) ? $_GET['dateDebut'] : null; ?>">
+                    <?= isset($erreurs['dateDebut']) ? $erreurs['dateDebut'] : null; ?>
+                </div>
+                <div style="padding: 10px;">
+                    <label for="date">Date fin offre :</label>
+                    <input type="date" name="dateFin" value="<?= isset($_GET['dateFin']) ? $_GET['dateFin'] : null; ?>">
+                    <?= isset($erreurs['dateFin']) ? $erreurs['dateFin'] : null; ?>
                 </div>
 
-                <div>
-                    <label for="image">Image :</label>
-                    <input type="text" name="image" value="<?= isset($_GET['image']) ? $_GET['image'] : null; ?>">
-                    <?= isset($erreurs['image']) ? $erreurs['image'] : null; ?>
+                <div style="padding: 10px;">
+                    <label for="nbMinPlace">Nombre Place Min Offre :</label>
+                    <input type="number" name="nbMinPlace" value="<?= isset($_GET['nbMinPlace']) ? $_GET['nbMinPlace'] : null; ?>">
+                    <?= isset($erreurs['nbMinPlace']) ? $erreurs['nbMinPlace'] : null; ?>
                 </div>
 
-                <div>
+                <div style="padding: 10px;">
                     <input type="submit" name="validation">
                 </div>
             </form>
+            <a href="offreview.php">Retour</a>
         </main>
+
     </body>
 </html>
