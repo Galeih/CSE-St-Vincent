7e410fe55bc978d8fdc86f40feb838e249172541
Florian Sauvage <florian.sauvage.etudiant@gmail.com>
2023-05-24 20:54:11 +0200

    helo 3

--- back.php
+++ back.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet">
     <link rel="icon" href="assets/sv_logo.png">
-    <title>CSE Saint-Vincent - Back - Acceuil</title>
+    <title>CSE Saint-Vincent - Back - Accueil</title>
 </head>
     <body>
         <header>
