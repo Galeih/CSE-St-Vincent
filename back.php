0a7941e868abf466c1cb326e6e0f0516ea86f033
Florian Sauvage <florian.sauvage.etudiant@gmail.com>
2023-05-24 19:14:47 +0200

    helo

--- back.php
+++ back.php
@@ -1,3 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet">
+    <link rel="icon" href="assets/sv_logo.png">
+    <title>CSE Saint-Vincent - Back - Acceuil</title>
+</head>
+    <body>
+        <header>
+            <div class="light-gray"></div>
+            <div class="blue">
+                <nav>
+                    <div class="logo"><img src="assets/logo_st_vincent_1.png" alt="logo_st_vincent"></div>
+                    <ul>
+                        <a href="back.php">
+                            <li class="active">
+                                Back-Accueil
+                            </li>
+                        </a>
+                        <a href="partview.php">
+                            <li>
+                                Back-Partenariats
+                            </li>
+                        </a>
+                        <a href="billetterieview.php">
+                            <li>
+                                Back-Billetterie
+                            </li>
+                        </a>
+                        <a href="contactview.php">
+                            <li>
+                                Back-Contact
+                            </li>
+                        </a>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main>
 
         </main>
     </body>
