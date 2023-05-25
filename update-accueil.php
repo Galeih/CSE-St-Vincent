03789aedbdb02537a59502d3f8eb21d2a733b280
Florian Sauvage <florian.sauvage.etudiant@gmail.com>
2023-05-25 02:29:44 +0200

    helo

--- update-accueil.php
+++ update-accueil.php
@@ -8,8 +8,7 @@ try {
 
 
 } catch (\Exception $exception) {
-  // debug erreur :
-  //var_dump($exception);
+
 }
 
 $erreurs = [];
@@ -38,31 +37,30 @@ if (empty($erreurs)) {
 
   } catch (\Exception $exception) {
     echo 'Erreur lors de l\'ajout de la demande de contact.';
-    // Debug de l'erreur :
     var_dump($exception->getMessage());
   }
 }
-//Récupération des données du Titre de la page d'accueil
+//Récupération du Titre de la page d'accueil
 $titreInfoAccueil = $connexion -> prepare('SELECT Titre_Info_Accueil FROM info_accueil');
 $titreInfoAccueil ->execute();
 $TitreAccueil = $titreInfoAccueil->fetch();
 $TitreAccueil = $TitreAccueil['Titre_Info_Accueil'];
-//Récupération des données de la description de la page d'accueil
+//Récupération de la description de la page d'accueil
 $texteInfoAccueil = $connexion -> prepare('SELECT Texte_Info_Accueil FROM info_accueil');
 $texteInfoAccueil ->execute();
 $TexteAccueil = $texteInfoAccueil->fetch();
 $TexteAccueil = $TexteAccueil['Texte_Info_Accueil'];
- //Récupération des données de Téléphone
+ //Récupération du numéro de Téléphone
  $tel = $connexion -> prepare('SELECT Num_Tel_Info_Accueil FROM info_accueil');
  $tel ->execute();
  $phone = $tel->fetch();
  $phone = $phone['Num_Tel_Info_Accueil'];
- //Récpération des données de l'email
+ //Récpération de l'email
  $email = $connexion -> prepare('SELECT Email_Info_Accueil FROM info_accueil');
  $email ->execute();
  $adresseEmail = $email->fetch();
  $adresseEmail = $adresseEmail['Email_Info_Accueil'];
- //Récupération des données de l'emplacement du bureau du CSE
+ //Récupération de l'emplacement du bureau du CSE
  $place = $connexion -> prepare('SELECT Emplacement_Bureau_Info_Accueil FROM info_accueil');
  $place ->execute();
  $office = $place->fetch();
@@ -74,38 +72,41 @@ $TexteAccueil = $TexteAccueil['Texte_Info_Accueil'];
   <head>
     <meta name="viewport"> 
     <title>CSE Saint-Vincent</title>
-    <link rel="shortcut icon" href="assets/Logo_parNodevo (1).png" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/7871bf468e.js" crossorigin="anonymous"></script>
     <meta charset="utf-8" />
   </head>
 
   <header>
-        <div class="gris">
-        </div>
+        <div class="light-gray"></div>
         <div class="blue">
-            <nav>
-                <ul>
-                    <a href="index.php">
-                        <li class="active">
-                            Accueil
-                        </li>
-                    </a>
-                    <a href="partenariats.php">
-                        <li>Partenariats</li>
-                    </a>
-                    <a href="billetterie.php">
-                        <li>Billetterie</li>
-                    </a>
-                    <a href="contact.php">
-                        <li>Contact</li>
-                    </a>
-                </ul>
-
-            </nav>
-        </div>
+                <nav>
+                    <div class="logo"><img src="assets/logo_st_vincent_1.png" alt="logo_st_vincent"></div>
+                    <ul>
+                    <a href="back.php">
+                            <li>
+                                Back-Accueil
+                            </li>
+                        </a>
+                        <a href="partview.php">
+                            <li>
+                                Back-Partenariats
+                            </li>
+                        </a>
+                        <a href="offreview.php">
+                            <li>
+                                Back-Billetterie
+                            </li>
+                        </a>
+                        <a href="contactview.php">
+                            <li>
+                                Back-Contact
+                            </li>
+                        </a>
+                    </ul>
+                </nav>
+            </div>
     </header>
         </header>
 
